616011859f4eaffbd7cfdc4afd1bc0d5f8d7e19a
Bruno B. Scholl <bruno.bbs3@hotmail.com>
2017-04-10 16:09:37 -0300

    Removes the prints

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -9,21 +9,14 @@ public class PercolationStats {
         results = new double[trials];
         this.n = n;
         T = trials;
-        StdDraw.enableDoubleBuffering();
         for (int i = 0; i < trials; i++) {
-            System.out.println("Iteração numero: " + i);
             Percolation perc = new Percolation(n);
             while (!perc.percolates()) {
                 int row = StdRandom.uniform(n);
                 int col = StdRandom.uniform(n);
                 perc.open(row, col);
-                //System.out.println("Abriu: " + row + " " + col);
-                PercolationVisualizer.draw(perc, n);
-                StdDraw.show();
             }
             results[i] = (double)perc.numberOfOpenSites()/(double)(n * n);
-            System.out.println("results[" + i + "]: " + results[i]);
-            StdDraw.pause(500);
         }
     }
 
